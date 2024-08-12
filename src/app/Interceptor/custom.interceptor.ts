b1d14fe9a62209793f7d56a82be898085fcc5998
Bishowjit <77982152+Bishow-jit@users.noreply.github.com>
2024-08-12 16:54:50 +0600

    Interceptor Updated to handle expired bearer token

--- src/app/Interceptor/custom.interceptor.ts
+++ src/app/Interceptor/custom.interceptor.ts
@@ -1,8 +1,13 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, throwError } from 'rxjs';
 
 export const customInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const toastr = inject(ToastrService) 
   const excludedUrls = ['/registration','/login'];
-
   // Check if the request URL is in the excluded URLs
   if (excludedUrls.some(url => req.url.includes(url))) {
     return next(req); // Skip the interceptor
@@ -17,7 +22,17 @@ export const customInterceptor: HttpInterceptorFn = (req, next) => {
           Authorization: `Bearer ${token}`
         }
       });
-      return next(clonereq);
+      return next(clonereq).pipe(
+        catchError((error:HttpErrorResponse)=>{
+          // console.log('statusCode==>',error.status )
+          if(error.status === 401 || error.status === 403){
+            localStorage.removeItem(token);
+            toastr.error('Please Login Again','Session Expired')
+            router.navigateByUrl('/login');
+          }
+          return throwError(()=>  error);
+        })
+      );
     }
   }
 
